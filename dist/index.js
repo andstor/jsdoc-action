467db1d376e0cd37bd192f9a2c8bfbc58bb969f3
André Storhaug <andr3.storhaug@gmail.com>
2020-02-19 01:45:18 +0100

    Correct path

--- dist/index.js
+++ dist/index.js
@@ -1482,12 +1482,13 @@ async function run() {
     args.push('-d', output_dir);
 
     core.info(`📝 Generating documentation`);
-    const jsdocPath = __webpack_require__.ab + "jsdoc";
-    console.log(__webpack_require__.ab + "jsdoc")
-    console.log(__webpack_require__.ab + "jsdoc")
-    console.log(__webpack_require__.ab + "jsdoc")
+    const jsdocPath = + '"' + __dirname + '/../node_modules/.bin/jsdoc' + '"';
+    console.log(path.join(__dirname, 'node_modules/.bin/jsdoc'))
+    console.log(path.resolve(__dirname, 'node_modules/.bin/jsdoc'))
+    console.log(path.resolve(__dirname, 'lol/omg/node_modules/.bin/jsdoc'))
+    console.log(jsdocPath)
     console.log('"' + jsdocPath + '"')
-    await exec.exec('"' + jsdocPath + '"', args);
+    await exec.exec(jsdocPath , args);
 
     core.info(`🎉 Documentation 📖 has ben generated to the ${output_dir} folder 📁`);
   }
