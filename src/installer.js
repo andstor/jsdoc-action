5295506b950c10892027f8cb3f5e5a4c80e47690
André Storhaug <andr3.storhaug@gmail.com>
2020-04-21 06:06:33 +0200

    Parse name of template package correctly

--- src/installer.js
+++ src/installer.js
@@ -1,16 +1,44 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 const path = require('path');
+const utils = require('./utils');
 
-async function install(template_name) {
-
+/**
+ * Installs a JSDoc template by executing `npm install template`.
+ * The template needs to be a JavaScript package.
+ * @param {string} template The JSDoc template to install.
+ * @async
+ * @returns {Promise<string>} The name of the installed template.
+ */
+async function installTemplate(template) {
+    let templateName = '';
     const actionDir = path.join(__dirname, '../');
-    let cmd = 'npm';
-    let args = ['install', template_name, '--production'];
 
-    core.info(`Installing JSDoc template: ${template_name}`);
-    await exec.exec(cmd, args, { cwd: actionDir });
+    let cmd = 'npm';
+    let args = ['install', template, '--production', '--parseable'];
+    core.info(`Installing JSDoc template: ${template}`);
     core.debug(`Command: ${cmd} ${args}`);
+    
+    let installOutput = '';
+    const options = {};
+    options.listeners = {
+        stdout: (data) => {
+            installOutput += data.toString();
+        }
+    };
+    options.cwd = actionDir;
+    
+    await exec.exec(cmd, args, options);
+    core.debug(`Installation output: ${installOutput}`);
+
+    let parsedString = installOutput.trim().split(/\s+/);
+    let packageLocation = parsedString[3]; // node_modules/folder
+    const filePath = path.join(__dirname, packageLocation + '/package.json');
+    templateName = await utils.getPackageName(filePath);
+
+    return templateName;
 }
 
-module.exports.install = install;
+module.exports = {
+    installTemplate
+};
